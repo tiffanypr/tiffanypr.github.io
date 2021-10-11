165298be916ce3e783879079c0768f9fa7600a4b
tiffanypr <89567393+tiffanypr@users.noreply.github.com>
2021-10-10 23:23:22 -0500

    Update future.js

--- week07/future.js
+++ week07/future.js
@@ -3,11 +3,11 @@ var city = ['Dallas','Houston','Lubbock','McAllen'];
 var spouse = ['Michelle','Valerie', 'Melissa','Lilly'];
 var numKids = [0,3,5,1];
 
-var xx = Math.floor(Math.random() * 4)
+var mm = Math.floor(Math.random() * 4)
 
-function fortune(job,city, spouse,numKids) {
+function fortune(job, city, spouse, numKids) {
 
 document.write("You will be a " + job + ' in ' + city + ', and married to ' + spouse + ' with ' + numKids + ' kids. ');
 }
 
-fortune (job[xx], city[xx], spouse[xx],numKids[xx])
+fortune (job[mm], city[mm], spouse[mm],numKids[mm])
